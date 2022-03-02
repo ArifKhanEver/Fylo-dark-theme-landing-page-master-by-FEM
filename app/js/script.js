e241a11aaaf2a4262aa05607a16742350f8eb590
Arif Khan <mdshafiqul0404@gmail.com>
2022-03-02 22:06:34 +0600

    completed

--- app/js/script.js
+++ app/js/script.js
@@ -3,14 +3,12 @@ const email = document.getElementById("email").value
 const errorMessage = document.getElementById("errorMessage")
 
 form.addEventListener('submit', e=> {
-    let regex = /^[a-z0-9_]+@[^ ]+\.[a-z]{2,3}$/;
-
-    
+    let regex = /^[a-z0-9_]+@[^ ]+\.[a-z]{2,3}$/;    
 
     if (email.match(regex)) {
         errorMessage.innerText = ""
     }else {
-        // e.preventDefault()
+        e.preventDefault()
         errorMessage.innerText = "Please enter a valid email address"
     }
 })
