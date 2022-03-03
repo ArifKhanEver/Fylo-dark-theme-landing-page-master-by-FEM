05f6290c2e9d351224a4a604bf5f3f62d589af5b
Arif Khan <mdshafiqul0404@gmail.com>
2022-03-03 14:01:42 +0600

    completed

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,26 @@
-const form = document.getElementById("form")
-const email = document.getElementById("email").value
-const errorMessage = document.getElementById("errorMessage")
 
-form.addEventListener('submit', e=> {
-    let regex = /^[a-z0-9_]+@[^ ]+\.[a-z]{2,3}$/;    
+const form = document.getElementById("form");
 
-    if (email.match(regex)) {
-        errorMessage.innerText = ""
-    }else {
-        e.preventDefault()
-        errorMessage.innerText = "Please enter a valid email address"
+form.addEventListener("submit", formValidation);
+
+function formValidation(event) {
+    event.preventDefault();
+
+    const email =document.getElementById('email');
+    const errMessage = document.getElementById("errorMessage")
+    const isValid = validateEmail(email.value);
+
+    if (!isValid) {
+        errMessage.classList.add("active-error-alert");
+        return;
+    } else {
+        errMessage.classList.remove("active-error-alert");
     }
-})
+
+    email.value = "";
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
